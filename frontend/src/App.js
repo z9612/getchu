e84a7a3969fdb913184425d3506545235c04283c
halim <rimha93@gmail.com>
2022-02-10 04:32:01 +0900

    fix: 자동완성 오류 수정중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,32 @@
-import { 
-  BrowserRouter as Router, 
-  Routes,  
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import BackBarLayout from './layouts/BackBarLayout';
+import BackBarLayout from "./layouts/BackBarLayout";
 import SurveyPage from "./pages/survey/SurveyPage";
-import Result from './pages/result';
-import Start from './pages/start'
-import EstimateDetailPage from './pages/estimate/EstimateDetailPage';
-import './index.css'
+import Result from "./pages/result";
+import Start from "./pages/start";
+import DogSelect from "./pages/dogSelect/dogSelect";
+import EstimateDetailPage from "./pages/estimate/EstimateDetailPage";
+import "./index.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/result' element={ <Result /> } />
-        <Route path='' element={ <BackBarLayout to='/Result' title='처음으로' /> } >
-          <Route path='/survey' element={ <SurveyPage /> } />
+        <Route path="/result" element={<Result />} />
+        <Route
+          path=""
+          element={<BackBarLayout to="/Result" title="처음으로" />}
+        >
+          <Route path="/survey" element={<SurveyPage />} />
         </Route>
-        <Route path='/estimate/detail' element={ <BackBarLayout to='/estimate' title='초기 자금 견적 내기' /> } >
-          <Route path='/estimate/detail' element={ <EstimateDetailPage /> } />
+        <Route
+          path="/estimate/detail"
+          element={<BackBarLayout to="/estimate" title="초기 자금 견적 내기" />}
+        >
+          <Route path="/estimate/detail" element={<EstimateDetailPage />} />
         </Route>
-        <Route path='/start' element={ <Start /> } />
+        <Route path="/start" element={<Start />} />
+        <Route path="/dogselect" element={<DogSelect />} />
       </Routes>
     </Router>
   );

9af36ccd3cb6821bfb886155bf590d06dfde8e30
adiens916 <adiens916@naver.com>
2022-02-09 11:21:16 +0900

    Feat: 추가 레이아웃에 제목 넣기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import BackBarLayout from './layouts/BackBarLayout';
 import SurveyPage from "./pages/survey/SurveyPage";
 import Result from './pages/result';
 import Start from './pages/start'
+import EstimateDetailPage from './pages/estimate/EstimateDetailPage';
 import './index.css'
 
 function App() {
@@ -15,8 +16,11 @@ function App() {
     <Router>
       <Routes>
         <Route path='/Result' element={ <Result /> } />
-        <Route path='' element={ <BackBarLayout to='/Result' /> } >
+        <Route path='' element={ <BackBarLayout to='/Result' title='처음으로' /> } >
           <Route path='/survey' element={ <SurveyPage /> } />
+        </Route>
+        <Route path='/estimate/detail' element={ <BackBarLayout to='/estimate' title='초기 자금 견적 내기' /> } >
+          <Route path='/estimate/detail' element={ <EstimateDetailPage /> } />
         </Route>
         <Route path='/start' element={ <Start /> } />
       </Routes>

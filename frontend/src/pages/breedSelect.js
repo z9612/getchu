a0625403e41fd8d5f042f44cb0cee08dd06c28e6
halim <rimha93@gmail.com>
2022-02-12 09:56:44 +0900

    Style: 디자인 추가

--- frontend/src/pages/breedSelect.js
+++ frontend/src/pages/breedSelect.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Container, Typography } from "@material-ui/core";
 import ComboBox from "./breedSelectComponent/ComboBox";
-
+import BreedSelectHeader from "./breedSelectComponent/breedSelectHeader";
 function dogSelect() {
   return (
     <Container fixed maxWidth="xs">
+      <BreedSelectHeader />
       <ComboBox />
     </Container>
   );

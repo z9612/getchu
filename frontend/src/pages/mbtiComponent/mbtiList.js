0a03636a5c880f7a632b6807a85f272b6c9bd19f
halim <rimha93@gmail.com>
2022-02-14 21:29:29 +0900

    Feat: mbti 연결 작업중

---
+++ frontend/src/pages/mbtiComponent/mbtiList.js
@@ -0,0 +1,23 @@
+const mbtiList = [
+  {
+    id: 1,
+    name: "ENFJ",
+    url: "img/ENFJ.jpeg",
+  },
+  {
+    id: 2,
+    name: "ENFJ",
+    url: "img/ENFP.jpeg",
+  },
+  {
+    id: 3,
+    name: "ENFJ",
+    url: "img/ENTJ.jpeg",
+  },
+  {
+    id: 4,
+    name: "ENFJ",
+    url: "img/ENTP.jpeg",
+  },
+];
+export default mbtiList;

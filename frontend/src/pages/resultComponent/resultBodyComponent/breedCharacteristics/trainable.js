c9cc4444f04a9dfbe86c9d33cc57d054f0811583
LEMONRIME <leejsl96@gmail.com>
2022-02-15 11:48:25 +0900

    Fix : 수정 결과페이지 버튼 위치 & 견종 특징 1차 번역

--- frontend/src/pages/resultComponent/resultBodyComponent/breedCharacteristics/trainable.js
+++ frontend/src/pages/resultComponent/resultBodyComponent/breedCharacteristics/trainable.js
@@ -5,29 +5,31 @@ const trainable = (props) => {
   return (
     <div>
       <div className='row-arrange'>
-        <div className='result-dog-info'>Easy_To_Train</div>
+        <div className='result-dog-info'>훈련이 쉬운가</div>
         <Rating name='test-rating' value={ props.dogData.trainable_Easy_To_Train } readOnly />
       </div>
       <div className='row-arrange'>
-        <div className='result-dog-info'>Intelligence</div>
+        <div className='result-dog-info'>지능</div>
         <Rating name='test-rating' value={ props.dogData.trainable_Intelligence } readOnly />
       </div>
       <div className='row-arrange'>
-        <div className='result-dog-info'>Potential_For_Mouthiness</div>
+        <div className='result-dog-info'>물 가능성? 무는 정도?</div>
         <Rating name='test-rating' value={ props.dogData.trainable_Potential_For_Mouthiness } readOnly />
       </div>
       <div className='row-arrange'>
-        <div className='result-dog-info'>Prey_Drive</div>
+        <div className='result-dog-info'>사냥감 쫓기</div>
         <Rating name='test-rating' value={ props.dogData.trainable_Prey_Drive } readOnly />
       </div>
+      <div className='explanation'>먹이를 찾고, 쫓고, 포획하는 본능적인 성향</div>
       <div className='row-arrange'>
-        <div className='result-dog-info'>Tendency_To_Bark_Or_Howl</div>
+        <div className='result-dog-info'>짖을 가능성</div>
         <Rating name='test-rating' value={ props.dogData.trainable_Tendency_To_Bark_Or_Howl } readOnly />
       </div>
       <div className='row-arrange'>
-        <div className='result-dog-info'>Wanderlust_Potential</div>
+        <div className='result-dog-info'>방랑벽</div>
         <Rating name='test-rating' value={ props.dogData.trainable_Wanderlust_Potential } readOnly />
       </div>
+      <div className='explanation'>이리저리 떠돌아다니기를 좋아하는가</div>
     </div>
   );
 };

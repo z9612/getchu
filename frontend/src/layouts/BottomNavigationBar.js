c4240ece0512cc2db510e9e68294987c1b9e7479
LEMONRIME <leejsl96@gmail.com>
2022-02-17 15:17:30 +0900

    Feat : 추가 bottom-navigation

---
+++ frontend/src/layouts/BottomNavigationBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
+import HomeIcon from '@mui/icons-material/Home';
+import PetsIcon from '@mui/icons-material/Pets';
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import MapIcon from '@mui/icons-material/Map';
+
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+const BottomNavigationBar = () => {
+
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box 
+      sx={{ 
+        position: 'fixed',
+        bottom: 0,
+        width: '100vw' 
+      }}>
+      <BottomNavigation
+        showLabels
+        // value={value}
+        // onChange={(event, newValue) => {
+        //   setValue(newValue);
+        // }}
+      >
+        <BottomNavigationAction label="견종추천" icon={<PetsIcon />} component={Link} to={'/recommend'} />
+        <BottomNavigationAction label="홈으로" icon={<HomeIcon />} component={Link} to={'/start'} />
+        <BottomNavigationAction label="초기견적" icon={<PriceCheckIcon />} component={Link} to={'/cost/breedselect'} />
+        <BottomNavigationAction label="Map" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+};
+
+export default BottomNavigationBar;
